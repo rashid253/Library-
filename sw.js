0a971d21b7925afa83e94c96cb3ae1d98bb954bb
rashid253 <rashidncit@gmail.com>
2026-01-29 17:00:30 +0500

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'business-card-v1';
+const urlsToCache = [
+  './',
+  './card.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
