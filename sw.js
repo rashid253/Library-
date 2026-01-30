f2075775918d85fb7c5ecc8f368586051e71ce9e
rashid253 <rashidncit@gmail.com>
2026-01-31 02:14:00 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
+const CACHE="card-v1";
+
 self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open("card-app").then(c=>{
-      return c.addAll(["card.html","manifest.json"]);
-    })
-  );
+ e.waitUntil(
+  caches.open(CACHE).then(c=>{
+   return c.addAll([
+    "index.html",
+    "card.html",
+    "manifest.json"
+   ]);
+  })
+ );
 });
 
 self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  );
+ e.respondWith(
+  caches.match(e.request).then(r=>{
+   return r || fetch(e.request);
+  })
+ );
 });
