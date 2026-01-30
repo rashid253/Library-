8716024b9889f425c28ca0c319bffe0fcbc847ec
rashid253 <rashidncit@gmail.com>
2026-01-30 21:39:06 +0500

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,13 @@
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open("card-app").then(c=>{
+      return c.addAll(["card.html","manifest.json"]);
+    })
+  );
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
